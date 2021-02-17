3b39b5666ba4465f63a627bc9cd972b9a2558461
Cool2hack <79107739+Cool2hack@users.noreply.github.com>
2021-02-17 14:15:00 +0300

    My the first commit

--- src/Glava7.java
+++ src/Glava7.java
@@ -1,10 +1,9 @@
-public class Glava7 {
-    public static void main(String[] args) {
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-    }
+public class Glava7 {
 }
 
-class TwoDShape {
+abstract class TwoDShape {
     private double widht;
     private double height;
     private String name;
@@ -13,16 +12,19 @@ class TwoDShape {
         widht = height = 0;
         name = "none";
     }
-
     TwoDShape(double widht, double height, String name){
         this.widht = widht;
         this.height = height;
         this.name = name;
     }
-
     TwoDShape(double x, String name){
         widht = height = x;
         this.name = name;
+    }
+    TwoDShape(TwoDShape ob){
+        widht = ob.getWidht();
+        height = ob.getHeight();
+        name = ob.getName();
     }
 
     public void setHeight(double height) {
@@ -44,10 +46,70 @@ class TwoDShape {
     void showDim (double widht, double height){
         System.out.println("Ширина и высота соответственно: " + widht + " " + height);
     }
+    abstract double area ();
+}
+
+class Triangle extends TwoDShape{
+    private String style;
+
+    Triangle(){
+        super();
+        style = "none";
+    }
+    Triangle(double x){
+        super(x, "треугольник");
+        style = "закрашенный";
+    }
+    Triangle(String s, double width, double height){
+        super(width,height,"треугольник");
+        style = s;
+    }
+    Triangle (Triangle ob){
+        super(ob);
+        style = ob.style;
+    }
+
+    double area () {
+        return getHeight() * getHeight() / 2;
+    }
+    void showStyle (){
+        System.out.println("Style: " +style);
+    }
+}
+
+class Rectangle extends TwoDShape{
+
+    Rectangle(){
+        super();
+    }
+    Rectangle(double width, double height){
+        super(width,height, "прямоугольник");
+    }
+    Rectangle(double x){
+        super(x, "квадрат");
+    }
+    Rectangle (Rectangle ob){
+        super(ob);
+    }
 
     double area (){
-        System.out.println("Этот метод необходимо переопределять в остальных подклассах");
-        return 0.0;
+        return getHeight() * getWidht();
+    }
+}
+
+class AbsShape {
+    public static void main(String[] args) {
+        TwoDShape twoDShape[] = new TwoDShape[4];
+        twoDShape[0] = new Triangle("контурный", 8, 12);
+        twoDShape[1] = new Rectangle(10);
+        twoDShape[2] = new Rectangle(10,4);
+        twoDShape[3] = new Triangle(7);
+
+        for (int i = 0; i < twoDShape.length; i++) {
+            System.out.println("Объект - " + twoDShape[i].getName());
+            System.out.println("Площадь - " + twoDShape[i].area());
+            System.out.println();
+        }
     }
 }
 
